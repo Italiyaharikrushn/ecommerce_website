eaa3c66728c3dc9ff00098486887a657ba924f61
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-12-11 12:59:04 +0530

    done

--- ecom/models.py
+++ ecom/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import date
 import uuid
 
 def get_image_upload_to(instance, filename):
@@ -7,41 +6,12 @@ def get_image_upload_to(instance, filename):
     filename = f"{uuid.uuid4()}.{ext}"
     return f"profile_images/{filename}"
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=14)
-    password = models.CharField(max_length=255)
-    gender = models.CharField(
-        max_length=10,
-        choices=[("Male", "Male"), ("Female", "Female"), ("Other", "Other")],
-        default="Select",
-    )
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to=get_image_upload_to)
 
     def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.phone.startswith("+91-"):
-            self.phone = f"+91-{self.phone}"
-        super().save(*args, **kwargs)
-
-# class Products(models.Model):
-#     product_name = models.CharField(max_length=100)
-#     description = models.CharField(max_length=500)
-#     price = models.BooleanField
-#     stock = models.IntegerField
-#     image = models.ImageField(upload_to=get_image_upload_to)
-
-# class Orders(models.Model):
-#     order_date = models.DateField(default=date.today)
-#     status = models
-#     total_price = models
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos', default=1)
-
-# class Payment(models.Model):
-#     Payment_date = models
-#     amount = models
-#     status = models
-#     Payment_method = models
-#     order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name='todos', default=1)
+        return self.product_name
--- ecom/views.py
+++ ecom/views.py
@@ -1,3 +1,23 @@
 # Create your views here.
 from django.shortcuts import render, redirect
+from .models import Product
 
+def add_product(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product_name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        stock = request.POST.get('stock')
+        image = request.FILES.get('image')
+
+        Product.objects.create(
+            product_name=product_name, description=description, price=price, stock=stock, image=image
+        )
+        return redirect('show_products')
+
+    return render(request, 'product_details/add_product.html')
+
+
+def show_products(request):
+    products = Product.objects.all()
+    return render(request, 'product_details/show_product.html', {'products': products})
