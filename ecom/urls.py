e30e8f8ae2f2108fe6fdfbabe03e40cad0da445d
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-12-19 11:09:29 +0530

    done

--- ecom/urls.py
+++ ecom/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home_view, name='home_view'),
     path('show-products/', views.show_products, name='show_products'),
-    path('add-product/', views.add_product, name='add_product'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
